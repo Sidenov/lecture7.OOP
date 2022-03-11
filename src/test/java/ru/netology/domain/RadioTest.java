aa57816bbfd9699e08918639387503f84fd84980
Александр Сиденов <sid_san@mail.ru>
2022-03-11 18:39:48 +0800

    Создание конструктора

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -14,8 +14,24 @@ class RadioTest {
             "'setRadioStation 10', 10, 0",
             "'setRadioStation 0', 0, 0",
             "'setRadioStation -1', -1, 0"})
-    void setRadioStation(String testName, int currentRadioStation, int expected) {
+    void setRadioStationShouldUseNoArgsConstructor(String testName, int currentRadioStation, int expected) {
         Radio station = new Radio();
+
+        station.setCurrentRadioStation(currentRadioStation);
+
+        int actual = station.getCurrentRadioStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"'setRadioStation 5', 5, 5",
+            "'setRadioStation 9', 9, 9",
+            "'setRadioStation 21', 21, 0",
+            "'setRadioStation 0', 0, 0",
+            "'setRadioStation -1', -1, 0"})
+    void setRadioStationShouldUseConstructor(String testName, int currentRadioStation, int expected) {
+        Radio station = new Radio(20);
 
         station.setCurrentRadioStation(currentRadioStation);
 
@@ -28,8 +44,23 @@ class RadioTest {
     @CsvSource(value = {"'nextRadioStation 6', 5, 6",
             "'nextRadioStation 0', 9, 0",
             "'nextRadioStation 1', 0, 1"})
-    void next(String testName, int currentRadioStation, int expected) {
+    void nextShouldUseNoArgsConstructor(String testName, int currentRadioStation, int expected) {
         Radio station = new Radio();
+
+        station.setCurrentRadioStation(currentRadioStation);
+        station.next();
+
+        int actual = station.getCurrentRadioStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"'nextRadioStation 6', 5, 6",
+            "'nextRadioStation 0', 12, 0",
+            "'nextRadioStation 1', 0, 1"})
+    void nextShouldUseConstructor(String testName, int currentRadioStation, int expected) {
+        Radio station = new Radio(13);
 
         station.setCurrentRadioStation(currentRadioStation);
         station.next();
@@ -43,8 +74,23 @@ class RadioTest {
     @CsvSource(value = {"'prevRadioStation 4', 5, 4",
             "'prevRadioStation 0', 1, 0",
             "'prevRadioStation 9', 0, 9"})
-    void prev(String testName, int currentRadioStation, int expected) {
+    void prevShouldUseNoArgsConstructor(String testName, int currentRadioStation, int expected) {
         Radio station = new Radio();
+
+        station.setCurrentRadioStation(currentRadioStation);
+        station.prev();
+
+        int actual = station.getCurrentRadioStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"'prevRadioStation 4', 5, 4",
+            "'prevRadioStation 0', 1, 0",
+            "'prevRadioStation 14', 0, 14"})
+    void prevShouldUseConstructor(String testName, int currentRadioStation, int expected) {
+        Radio station = new Radio(15);
 
         station.setCurrentRadioStation(currentRadioStation);
         station.prev();
@@ -57,7 +103,7 @@ class RadioTest {
     @ParameterizedTest
     @CsvSource(value = {"'setVolumeRadio 5', 5, 5",
             "'setVolumeRadio 10', 10, 10",
-            "'setVolumeRadio 11', 11, 0",
+            "'setVolumeRadio 101', 101, 0",
             "'setVolumeRadio 0', 0, 0",
             "'setVolumeRadio -1', -1, 0"})
     void setVolume(String testName, int currentRadioStation, int expected) {
@@ -72,7 +118,7 @@ class RadioTest {
 
     @ParameterizedTest
     @CsvSource(value = {"'volumeUp to 6', 5, 6",
-            "'volumeUp to 11', 10, 10",
+            "'volumeUp to 101', 100, 100",
             "'volumeUp to 1', 0, 1"})
     void volumeUp(String testName, int currentRadioStation, int expected) {
         Radio volume = new Radio();
@@ -86,7 +132,7 @@ class RadioTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"'volumeDown to 5', 6, 5",
+    @CsvSource(value = {"'volumeDown to 99', 100, 99",
             "'volumeDown to 0', 1, 0",
             "'volumeDown to -1', 0, 0"})
     void volumeDown(String testName, int currentRadioStation, int expected) {
